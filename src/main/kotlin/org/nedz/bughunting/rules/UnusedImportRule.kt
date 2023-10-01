75320fa94b2627b0ca4701bb316eedc57fcf24da
Margarita Nedzelska <margarita.nedzelska@sonarsource.com>
2023-10-01 23:26:01 +0200

    Refactoring v2

--- src/main/kotlin/org/nedz/bughunting/rules/UnusedImportRule.kt
+++ src/main/kotlin/org/nedz/bughunting/rules/UnusedImportRule.kt
@@ -1,13 +1,6 @@
 package org.nedz.bughunting.rules
 
 import org.jetbrains.kotlin.psi.KtFile
-import org.jetbrains.kotlin.psi.KtImportList
-import org.jetbrains.kotlin.psi.KtPackageDirective
-import org.jetbrains.kotlin.psi.KtSimpleNameExpression
-import org.jetbrains.kotlin.psi.psiUtil.collectDescendantsOfType
-import org.jetbrains.kotlin.resolve.BindingContext
-import org.jetbrains.kotlin.resolve.descriptorUtil.fqNameOrNull
-import org.jetbrains.kotlin.resolve.descriptorUtil.getImportableDescriptor
 import org.nedz.bughunting.api.FileContext
 import org.nedz.bughunting.api.Rule
 
