8e77c89f9b0173d9cc4ffb03f50739eccb6d3f44
Marharyta Nedzelska <margarita.nedzelska@sonarsource.com>
2025-04-29 15:14:16 +0200

    Add Analysis Api dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.9.0"
-    kotlin("plugin.serialization") version "1.9.0"
+    kotlin("jvm") version "2.0.20"
+    kotlin("plugin.serialization") version "2.0.20"
     application
 }
 
@@ -9,9 +9,34 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven(url = "https://maven.pkg.jetbrains.space/kotlin/p/kotlin/kotlin-ide-plugin-dependencies") {
+        content {
+            includeGroup("org.jetbrains.kotlin")
+        }
+    }
 }
 
 dependencies {
+    listOf(
+        // Source of these artifacts is
+        // https://github.com/JetBrains/kotlin/tree/v2.0.21/prepare/ide-plugin-dependencies
+        // where ones whose name contains "high-level" are deprecated and should not be used - see
+        // https://github.com/JetBrains/kotlin/commit/3ad9798a17ad9eb68cdb1e9f8f1a69584151bfd4
+        "org.jetbrains.kotlin:analysis-api-standalone-for-ide",
+        "org.jetbrains.kotlin:analysis-api-platform-interface-for-ide",
+        "org.jetbrains.kotlin:analysis-api-for-ide", // old name "high-level-api-for-ide"
+        "org.jetbrains.kotlin:analysis-api-impl-base-for-ide", // old name "high-level-api-impl-base"
+        "org.jetbrains.kotlin:analysis-api-k2-for-ide", // old name "high-level-api-k2"
+        "org.jetbrains.kotlin:low-level-api-fir-for-ide",
+        "org.jetbrains.kotlin:symbol-light-classes-for-ide"
+    ).forEach {
+        api("$it:2.0.20") {
+            // https://youtrack.jetbrains.com/issue/KT-61639/Standalone-Analysis-API-cannot-find-transitive-dependencies
+            isTransitive = false
+        }
+    }
+    implementation("com.github.ben-manes.caffeine:caffeine:2.9.3")
+
     implementation( "org.jetbrains.kotlin", "kotlin-compiler", "2.0.20")
     implementation("com.google.code.gson:gson:2.8.9")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
